abfbaa0012bbcafad76257286b58acd3deabf08d
andresusanto <andre@andresusanto.info>
2015-09-05 00:17:38 +0700

    Algoritma 1

--- app/src/main/java/com/ganesus/equaligram/MainActivity.java
+++ app/src/main/java/com/ganesus/equaligram/MainActivity.java
@@ -16,6 +16,8 @@ import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
     private ByteBuffer buffBitmap = null;
+    private ByteBuffer buffBitmapGray = null;
+    private ByteBuffer buffBitmap1 = null;
 
     public  void onClicker(View v){
 
@@ -26,20 +28,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = (TextView) findViewById(R.id.tv);
         buffBitmap = loadBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.lena));
+        buffBitmapGray = createGrayscale(buffBitmap);
+        buffBitmap1 = applyAlgo1(buffBitmapGray);
 
         Bitmap.Config conf = Bitmap.Config.ARGB_8888;
-        Bitmap canvas = Bitmap.createBitmap(780, 500, conf);
+        Bitmap canvas = Bitmap.createBitmap(780, 320, conf);
 
-        Bitmap tes = genHistogram(buffBitmap, canvas);
 
-        ImageView iv = (ImageView) findViewById(R.id.imageView);
-        iv.setImageBitmap(tes);
+
+        ImageView ivAsli = (ImageView) findViewById(R.id.ivAsli);
+        ImageView hisAsli = (ImageView) findViewById(R.id.hisAsli);
+        ImageView hisGray = (ImageView) findViewById(R.id.hisGray);
+        ImageView ivGray = (ImageView) findViewById(R.id.ivGray);
+        ImageView hisAlgo1 = (ImageView) findViewById(R.id.hisAlgo1);
+        ImageView ivAlgo1= (ImageView) findViewById(R.id.ivAlgo1);
+
+
+        ivAsli.setImageBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.lena));
+        hisAsli.setImageBitmap(genHistogram(buffBitmap, canvas, false));
+        ivGray.setImageBitmap(createGrayscaleBmp(buffBitmap));
+        ivAlgo1.setImageBitmap(applyAlgo1Bmp(buffBitmapGray));
+
+        canvas = Bitmap.createBitmap(520, 320, conf);
+        hisGray.setImageBitmap(genHistogram(buffBitmapGray, canvas, true));
+
+        canvas = Bitmap.createBitmap(520, 320, conf);
+        hisAlgo1.setImageBitmap(genHistogram(buffBitmap1, canvas, true));
     }
 
     public native ByteBuffer loadBitmap(Bitmap bitmap);
-    public native Bitmap genHistogram(ByteBuffer bitmem, Bitmap canvas);
+    public native ByteBuffer createGrayscale(ByteBuffer bitmem);
+    public native ByteBuffer applyAlgo1(ByteBuffer bitmem);
+
+    public native Bitmap genHistogram(ByteBuffer bitmem, Bitmap canvas, boolean isGrayScale);
+    public native Bitmap createGrayscaleBmp(ByteBuffer bitmem);
+    public native Bitmap applyAlgo1Bmp(ByteBuffer bitmem);
 
     static {
         System.loadLibrary("equaligram");
--- app/src/main/jni/equaligram.cpp
+++ app/src/main/jni/equaligram.cpp
@@ -12,13 +12,17 @@
 #define  LOG_TAG    "EQUALIZER INSTAGRAM"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
-
+#define NOT_SPECIFIED_COLOR -1
 
 // supaya bisa dipanggil sama java
 extern "C"
 {
 	JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_loadBitmap (JNIEnv * env, jobject obj, jobject bitmap);
-	JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_genHistogram (JNIEnv * env, jobject obj, jobject bitmem, jobject canvas);
+	JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_genHistogram (JNIEnv * env, jobject obj, jobject bitmem, jobject canvas, bool isGrayScale);
+	JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_createGrayscale (JNIEnv * env, jobject obj, jobject bitmem);
+	JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_createGrayscaleBmp (JNIEnv * env, jobject obj, jobject bitmem);
+	JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_applyAlgo1 (JNIEnv * env, jobject obj, jobject bitmem);
+	JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_applyAlgo1Bmp (JNIEnv * env, jobject obj, jobject bitmem);
 }
 
 
@@ -28,12 +32,20 @@ extern "C"
 
 class NativeBitmap{
 	public:
-	uint32_t* pixels;
-	AndroidBitmapInfo bitmapInfo;
-	
-	NativeBitmap(){
-		pixels = NULL;
-	}
+		uint32_t* pixels;
+		AndroidBitmapInfo bitmapInfo;
+
+		NativeBitmap(){
+			pixels = NULL;
+		}
+
+		NativeBitmap(NativeBitmap* nBitmap){
+			this->bitmapInfo = nBitmap->bitmapInfo;
+			uint32_t pixelsCount = this->bitmapInfo.height * this->bitmapInfo.width;
+
+			this->pixels = new uint32_t[pixelsCount];
+			memcpy(this->pixels, nBitmap->pixels, sizeof(uint32_t) * pixelsCount);
+		}
 };
 
 typedef struct{
@@ -51,6 +63,26 @@ void convertIntToArgb(uint32_t pixel, ARGB* argb){
 	argb->alpha = ((pixel >> 24) & 0xff);
 }
 
+NativeBitmap* grayscaleBitmap(NativeBitmap* source){
+	NativeBitmap* result = new NativeBitmap(source);
+	uint32_t nBitmapSize = source->bitmapInfo.height * source->bitmapInfo.width;
+
+
+	for (uint32_t i = 0; i < nBitmapSize; i++){
+		ARGB bitmapColor;
+		convertIntToArgb(source->pixels[i], &bitmapColor);
+
+		uint8_t grayscaleColor = (int)(0.2989f * bitmapColor.red + 0.5870f * bitmapColor.green + 0.1141 * bitmapColor.blue);
+
+		bitmapColor.red = grayscaleColor;
+		bitmapColor.green = grayscaleColor;
+		bitmapColor.blue = grayscaleColor;
+
+		result->pixels[i] = convertArgbToInt(bitmapColor);
+	}
+
+	return result;
+}
 
 NativeBitmap* convertBitmapToNative(JNIEnv * env, jobject bitmap){
 	AndroidBitmapInfo bitmapInfo;
@@ -122,6 +154,108 @@ jobject convertNativeToBitmap(JNIEnv * env, NativeBitmap* nBitmap){
 
 }
 
+uint32_t* createHistogram(NativeBitmap* nBitmap){
+	if (nBitmap->pixels == NULL)
+    	return NULL;
+
+	uint32_t* result = new uint32_t[256];
+
+	for (int i=0; i<256; i++)
+		result[i] = 0;
+
+	uint32_t nBitmapSize = nBitmap->bitmapInfo.height * nBitmap->bitmapInfo.width;
+
+	for (uint32_t i = 0; i < nBitmapSize; i++){
+		ARGB bitmapColor;
+		convertIntToArgb(nBitmap->pixels[i], &bitmapColor);
+		result[bitmapColor.red]++;
+	}
+
+	return result;
+}
+
+NativeBitmap* transformNativeBitmap(NativeBitmap* source, uint32_t* transform){
+	NativeBitmap* result = new NativeBitmap(source);
+	uint32_t nBitmapSize = source->bitmapInfo.height * source->bitmapInfo.width;
+
+
+	for (uint32_t i = 0; i < nBitmapSize; i++){
+		ARGB bitmapColor;
+		convertIntToArgb(source->pixels[i], &bitmapColor);
+
+		bitmapColor.red = transform[bitmapColor.red];
+		bitmapColor.green = transform[bitmapColor.green];
+		bitmapColor.blue = transform[bitmapColor.blue];
+
+		result->pixels[i] = convertArgbToInt(bitmapColor);
+	}
+
+	return result;
+}
+
+
+///////////////////////////// ALGORITMA 1 ///////////////////////////////////
+
+
+uint32_t* get_cumulative_histogram(uint32_t* histogram) {
+
+	uint32_t* cumulative_histogram = new uint32_t[256];
+
+	int accumulation = 0;
+	for (int i=0; i<256; i++) {
+		accumulation += histogram[i];
+		cumulative_histogram[i] = accumulation;
+	}
+
+	return cumulative_histogram;
+}
+
+int get_lower_bound(uint32_t* histogram) {
+	for (int i=0;i<256;i++) {
+		if (histogram[i] !=0) {
+			return i;
+		}
+	}
+	return NOT_SPECIFIED_COLOR;
+}
+
+int get_upper_bound(uint32_t* histogram) {
+	for (int i= 255; i>=0; i--) {
+		if (histogram[i] !=0) {
+			return i;
+		}
+	}
+
+	return NOT_SPECIFIED_COLOR;
+}
+
+uint32_t* cumulative_equalization(uint32_t* histogram) {
+
+	uint32_t* color_transform = new uint32_t[256];
+	uint32_t* cumulative_histogram = get_cumulative_histogram(histogram);
+
+	int lower_bound = get_lower_bound(histogram);
+	int upper_bound = get_upper_bound(histogram);
+
+	for (int i=0; i<256; i++) {
+		color_transform[i] = NOT_SPECIFIED_COLOR;
+	}
+
+	int total_pixels = cumulative_histogram[upper_bound];
+	int denominator = total_pixels - cumulative_histogram[lower_bound];
+	for (int i=lower_bound;i<=upper_bound;i++) {
+		if (histogram[i])
+			color_transform[i] = ((cumulative_histogram[i] - cumulative_histogram[lower_bound]) *
+				(254) / denominator) + 1;
+	}
+
+	return color_transform;
+}
+
+
+//////////////////////// END ALGORITMA 1 //////////////////////////////////////
+
+
 /////////////////////////////////////////////////////////////////////////////////////
 // fungsi untuk load bitmap dan store ke native memory
 /////////////////////////////////////////////////////////////////////////////////////
@@ -130,14 +264,14 @@ JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_loadBitmap (J
     return env->NewDirectByteBuffer(convertBitmapToNative (env, bitmap), 0);
 }
 
-
 /////////////////////////////////////////////////////////////////////////////////////
-// fungsi untuk generate histogram
+// fungsi untuk generate histogram 3 warna
 /////////////////////////////////////////////////////////////////////////////////////
 
-JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_genHistogram (JNIEnv * env, jobject obj, jobject bitmem, jobject canvas){
+JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_genHistogram (JNIEnv * env, jobject obj, jobject bitmem, jobject canvas, bool isGrayScale){
 	NativeBitmap* nCanvas = convertBitmapToNative(env, canvas);
 	NativeBitmap* nBitmap = (NativeBitmap*) env->GetDirectBufferAddress(bitmem);
+
 
 	if (nBitmap->pixels == NULL || nCanvas->pixels == NULL)
 		return NULL;
@@ -149,6 +283,7 @@ JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_genHistogram 
 	for (uint16_t i = 0; i < 256; i++){
 		hRed[i] = 0; hGreen[i] = 0; hBlue[i] = 0;
 	}
+
 
 	uint32_t nBitmapSize = nBitmap->bitmapInfo.height * nBitmap->bitmapInfo.width;
 
@@ -162,44 +297,96 @@ JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_genHistogram 
 	}
 
 	float scalingBlue = 0.0f, scalingGreen = 0.0f, scalingRed = 0.0f;
-	uint32_t blue_max = hBlue[1], green_max = hGreen[1], red_max = hRed[1];
+	uint32_t blue_max = hBlue[0], green_max = hGreen[0], red_max = hRed[0];
 
 
-	for (uint16_t i = 1; i < 256; i++){
+	for (uint16_t i = 0; i < 256; i++){
 		if (hBlue[i] > blue_max) blue_max = hBlue[i];
 		if (hGreen[i] > green_max) green_max = hGreen[i];
         if (hRed[i] > red_max) red_max = hRed[i];
 	}
 
 	scalingBlue = 255.0f / blue_max; scalingGreen = 255.0f / green_max; scalingRed = 255.0f / red_max;
-	
-	ARGB aRed, aGreen, aBlue;
+
+	ARGB aRed, aGreen, aBlue, aGray;
 	aRed.red = 255; aRed.green = 0; aRed.blue = 0; aRed.alpha = 255;
 	aGreen.red = 0; aGreen.green = 255; aGreen.blue = 0; aGreen.alpha = 255;
 	aBlue.red = 0; aBlue.green = 0; aBlue.blue = 255; aBlue.alpha = 255;
+	aGray.red = 127; aGray.green = 127; aGray.blue = 127; aGray.alpha = 255;
 
 	uint32_t iRed= convertArgbToInt(aRed);
 	uint32_t iGreen= convertArgbToInt(aGreen);
 	uint32_t iBlue= convertArgbToInt(aBlue);
+	uint32_t iGray= convertArgbToInt(aGray);
 
-	for (uint16_t i = 1; i < 256; i++){
-		int barSize = (int) (hRed[i] * scalingRed);
-		for (int j = 0; j < barSize; j++){
-			nCanvas->pixels[i + (300 - j) * nCanvas->bitmapInfo.width] = iRed;
+	if (isGrayScale){
+		for (uint16_t i = 0; i < 256; i++){
+			int barSize = (int) (hRed[i] * scalingRed);
+			for (int j = 0; j < barSize; j++){
+				nCanvas->pixels[i * 2 + (300 - j) * nCanvas->bitmapInfo.width] = iGray;
+				nCanvas->pixels[i * 2 + 1 + (300 - j) * nCanvas->bitmapInfo.width] = iGray;
+			}
 		}
+	}else{
+		for (uint16_t i = 0; i < 256; i++){
+			int barSize = (int) (hRed[i] * scalingRed);
+			for (int j = 0; j < barSize; j++){
+				nCanvas->pixels[i + (300 - j) * nCanvas->bitmapInfo.width] = iRed;
+			}
 
-		barSize = (int) (hGreen[i] * scalingGreen);
-		for (int j = 0; j < barSize; j++){
-			nCanvas->pixels[260 + i + (300 - j) * nCanvas->bitmapInfo.width] = iGreen;
-		}
+			barSize = (int) (hGreen[i] * scalingGreen);
+			for (int j = 0; j < barSize; j++){
+				nCanvas->pixels[260 + i + (300 - j) * nCanvas->bitmapInfo.width] = iGreen;
+			}
 
-		barSize = (int) (hBlue[i] * scalingBlue);
-		for (int j = 0; j < barSize; j++){
-			nCanvas->pixels[520 + i + (300 - j) * nCanvas->bitmapInfo.width] = iBlue;
+			barSize = (int) (hBlue[i] * scalingBlue);
+			for (int j = 0; j < barSize; j++){
+				nCanvas->pixels[520 + i + (300 - j) * nCanvas->bitmapInfo.width] = iBlue;
+			}
 		}
 	}
-
-	LOGE("Sudah!");
+	//return convertNativeToBitmap(env, grayscaleBitmap(nBitmap));
 	return convertNativeToBitmap(env, nCanvas);
 
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+// fungsi untuk generate grayscale bitmap
+/////////////////////////////////////////////////////////////////////////////////////
+
+JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_createGrayscaleBmp (JNIEnv * env, jobject obj, jobject bitmem){
+	NativeBitmap* nBitmap = (NativeBitmap*) env->GetDirectBufferAddress(bitmem);
+	return convertNativeToBitmap(env, grayscaleBitmap(nBitmap));
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+// fungsi untuk generate grayscale native bitmap
+/////////////////////////////////////////////////////////////////////////////////////
+
+JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_createGrayscale (JNIEnv * env, jobject obj, jobject bitmem){
+	NativeBitmap* nBitmap = (NativeBitmap*) env->GetDirectBufferAddress(bitmem);
+	return env->NewDirectByteBuffer(grayscaleBitmap(nBitmap), 0);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+// fungsi untuk menerapkan algoritma 1 ke native bitmap grayscale
+/////////////////////////////////////////////////////////////////////////////////////
+
+JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_applyAlgo1 (JNIEnv * env, jobject obj, jobject bitmem){
+	NativeBitmap* nBitmap = (NativeBitmap*) env->GetDirectBufferAddress(bitmem);
+
+	uint32_t* histogram = createHistogram(nBitmap);
+	uint32_t* color_transform = cumulative_equalization(histogram);
+
+	return env->NewDirectByteBuffer(transformNativeBitmap(nBitmap, color_transform), 0);
+}
+
+JNIEXPORT jobject JNICALL Java_com_ganesus_equaligram_MainActivity_applyAlgo1Bmp (JNIEnv * env, jobject obj, jobject bitmem){
+	NativeBitmap* nBitmap = (NativeBitmap*) env->GetDirectBufferAddress(bitmem);
+
+	uint32_t* histogram = createHistogram(nBitmap);
+	uint32_t* color_transform = cumulative_equalization(histogram);
+
+	return convertNativeToBitmap(env, transformNativeBitmap(nBitmap, color_transform));
 }
